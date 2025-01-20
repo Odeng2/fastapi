ffe04786fcda012273de2c87d22451986c39c35a
Odeng2 <151814873+Odeng2@users.noreply.github.com>
2025-01-20 23:49:08 +0900

    handling errors

--- README.md
+++
@@ -1,2 +0,0 @@
-# fastapi_lec
-"실전! FastAPI 입문" 강의 실습
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException
 from pydantic import BaseModel
 
 
@@ -38,7 +38,7 @@ def get_todos_handler() :
 """
 
 #query parameter: 특정 값을 이용해 추가적인 작업 가능
-@app.get("/todos")
+@app.get("/todos", status_code=200)
 def get_todos_handler(order: str | None = None) :   #None = None: request시에 order값을 넣지 않아도 되도록 해주는 것
     ret = list(todo_data.values())
     if order and order == "DESC":   #order가 DESC일 경우, 내림차순으로 반환
@@ -47,9 +47,12 @@ def get_todos_handler(order: str | None = None) :   #None = None: request시에 
 
 
 #GET API: 단일 조회
-@app.get("/todos/{todo_id}")   #중괄호 안에 적으면 path로 이용 가능(subpath)
+@app.get("/todos/{todo_id}", status_code=200)   #중괄호 안에 적으면 path로 이용 가능(subpath)
 def get_todo_handler(todo_id: int):   #중괄호 안에 넣었던 걸 인자로 전달받을 수 있음
-    return todo_data.get(todo_id, {})
+    todo = todo_data.get(todo_id)
+    if todo:
+        return todo   #todo가 있는 경우, todo 반환
+    raise HTTPException(status_code=404, detail="ToDo Not Found")   #todo가 없는 경우, 404 에러 메시지 반환
 
 
 #POST API: todo 생성
@@ -59,14 +62,14 @@ class CreateToDoRequest(BaseModel):
     is_done: bool
 
 
-@app.post("/todos")
+@app.post("/todos", status_code=201)
 def create_todo_handler(request: CreateToDoRequest):
     todo_data[request.id] = request.dict()   #request는 class 객체이고, todo_data는 dictionary 형태이기 때문에 형태 변환 필요함
     return todo_data[request.id]
 
 
 #PATCH API: todo 수정
-@app.patch("/todos/{todo_id}")
+@app.patch("/todos/{todo_id}", status_code=200)
 def update_todo_handler(
         todo_id: int,
         is_done: bool = Body(..., embed=True),   #fastapi에서 하나의 컬럼 값은 request body를 읽어서 사용할 수 있음.
@@ -75,11 +78,13 @@ def update_todo_handler(
     if todo:
         todo["is_done"] = is_done
         return todo
-    return {}
+    raise HTTPException(status_code=404, detail="ToDo Not Found")
 
 
 #DELETE API: todo 삭제
-@app.delete("/todos/{todo_id}")
+@app.delete("/todos/{todo_id}", status_code=204)
 def delete_todo_handler(todo_id: int):
-    todo_data.pop(todo_id, None)
-    return todo_data   #원래는 status code를 활용해 별다른 응답을 하지 않지만, 실습에서는 삭제 후의 데이터를 보여줄 예정
+    todo = todo_data.pop(todo_id, None)
+    if todo:
+        return
+    raise HTTPException(status_code=404, detail="ToDo Not Found")
